ccfc66e692859bec1090a30c590271adad9d8d7a
felipe <felipebaise@gmail.com>
2025-10-20 20:19:53 -0300

    terceiro commit

--- Nova pasta/desafio03.py
+++ Nova pasta/desafio03.py
@@ -7,4 +7,4 @@ cupom ='PAULAE10'
 
 print(f'ola {nome} .Em {mes}  voce realizou uma compra no valor de R${valor: .2f}'
 f'e ganhou um desconto de {desconto}% em sua proxima compra'
-f'so usar o cupom{cupom}')
+f'so usar o cupom{cupom}.')
